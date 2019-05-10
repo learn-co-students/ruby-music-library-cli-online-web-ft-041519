9ccbf84dbb36d8b9a848b5332921ba4a3a29292f
Alison Golden <alisong779@yahoo.com>
2019-05-10 17:10:24 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -27,9 +27,11 @@ class MusicLibraryController
         elsif input == "list genres"
           list_genres
         elsif input == "list artist"
-          list_artist
+          list_songs_by_artist
         elsif input == "list genre"
-          list_genre
+          list_songs_by_genre
+        elsif input == "play song"
+          play_song
       end 
         end 
   end 
@@ -41,32 +43,49 @@ class MusicLibraryController
     end 
     
     def list_artists
-      a_sorted = Artist.all.sort {|a,b| a.name <=> b.name}
+      a_sorted = Artist.all.uniq.sort {|a,b| a.name <=> b.name}
       a_sorted.each.with_index(1) do |artist, index| 
         puts "#{index}. #{artist.name}"
       end 
     end 
    
    def list_genres
-    # binding.pry 
-     g_sorted = Genre.all.sort {|a,b| a.name <=> b.name}
-     g_sorted.each_with_index(1) do |genre, index| 
+     g_sorted = Genre.all.uniq.sort {|a,b| a.name <=> b.name}
+     g_sorted.each.with_index(1) do |genre, index| 
        puts "#{index}. #{genre.name}"
      end 
    end 
    
   def list_songs_by_artist
-    puts "Please enter the name of an aritst:"
+    puts "Please enter the name of an artist:"
     input = gets.strip
-    
+       if artist = Artist.find_by_name(input)
+          artist.songs.sort { |a,b| a.name <=> b.name }.each.with_index(1) do |song, index|
+            puts "#{index}. #{song.name} - #{song.genre.name}"
+        end 
+      end 
   end 
 
-   
    def list_songs_by_genre 
       puts "Please enter the name of a genre:"
       input = gets.strip
+      if genre = Genre.find_by_name(input)
+          genre.songs.sort { |a,b| a.name <=> b.name }.each.with_index(1) do |song, index|
+            puts "#{index}. #{song.artist.name} - #{song.name}"
+      end
+      end 
    end 
    
    def play_song
-   end 
+     puts "Which song number would you like to play?"
+     input = gets.chomp.to_i
+
+      songs = Song.all
+   
+      if (1..songs.length).include?(input)
+      song = Song.all.sort{ |a, b| a.name <=> b.name }[input - 1]
+    end
+     puts "Playing #{song.name} by #{song.artist.name}" if song
+    end
+   
 end 
