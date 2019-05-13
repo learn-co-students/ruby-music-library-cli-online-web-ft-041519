231436297baf8f8ce2e911081770b9575268c5bb
Erin Stalzer <erinstalzer@gmail.com>
2019-05-13 02:43:02 +0000

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -1,6 +1,6 @@
 class MusicLibraryController
 
-  attr_reader :path
+  attr_reader :path, :input
 
   def initialize(path="./db/mp3s")
     @path = path
@@ -18,17 +18,28 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    4.times {input = gets}
+    @input = gets
+    3.times {input = gets}
+    if @input == "list songs"
+      list_songs
+    elsif @input == "list artists"
+      list_artists
+    elsif @input == "list genres"
+      list_genres
+    elsif @input == "list artist"
+      list_songs_by_artist
+    elsif @input == "list genre"
+      list_songs_by_genre
+    elsif @input == "play song"
+      play_song
+    end
   end
 
   def list_songs
     song_arr = []
-    Song.all.each do |song|
-      song_arr << song.name
-    end
-    song_arr.sort!
+    Song.all.each { |song| song_arr << song.name }
     num = 0
-    song_arr.each do |song_name|
+    song_arr.sort!.each do |song_name|
       full_song = Song.find_by_name(song_name)
       num += 1
       puts "#{num}. #{full_song.artist.name} - #{full_song.name} - #{full_song.genre.name}"
@@ -37,30 +48,22 @@ class MusicLibraryController
 
   def list_artists
     artist_arr = []
-    Artist.all.each do |artist|
-      artist_arr << artist.name
-    end
-    artist_arr.sort!
+    Artist.all.each { |artist| artist_arr << artist.name }
     num = 0
-    artist_arr.each do |artist_name|
+    artist_arr.sort!.each do |artist_name|
       full_artist = Artist.find_by_name(artist_name)
       num += 1
-
       puts "#{num}. #{full_artist.name}"
     end
   end
 
   def list_genres
     genre_arr = []
-    Genre.all.each do |genre|
-      genre_arr << genre.name
-    end
-    genre_arr.sort!
+    Genre.all.each { |genre| genre_arr << genre.name }
     num = 0
-    genre_arr.each do |genre_name|
+    genre_arr.sort!.each do |genre_name|
       full_genre = Genre.find_by_name(genre_name)
       num += 1
-
       puts "#{num}. #{full_genre.name}"
     end
   end
@@ -69,15 +72,47 @@ class MusicLibraryController
     puts "Please enter the name of an artist:"
     user_input = gets
     artist = Artist.all.detect { |artist| artist.name == user_input }
-    song_arr = []
-    artist.songs.each do |song|
-      song_arr << song
+    if artist != nil
+      song_arr = []
+      artist.songs.each do |song|
+        song_arr << song.name
+      end
+      num = 0
+      song_arr.sort!.each do |song_name|
+        num += 1
+        puts "#{num}. #{song_name} - #{Song.find_by_name(song_name).genre.name}"
+      end
     end
-    song_arr.sort!
-    num = 0
-    song_arr.each do |song_name|
-      num += 1
-      puts "#{num}. #{song_name} - #{find_by_name(song_name).genre}"
+  end
+
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    user_input = gets
+    genre = Genre.all.detect { |genre| genre.name == user_input }
+    if genre != nil
+      song_arr = []
+      genre.songs.each do |song|
+        song_arr << song.name
+      end
+      num = 0
+      song_arr.sort!.each do |song_name|
+        num += 1
+        puts "#{num}. #{Song.find_by_name(song_name).artist.name} - #{song_name}"
+      end
+    end
+  end
+
+  def play_song
+    puts "Which song number would you like to play?"
+    input = gets
+    if input != nil
+      song_list = Song.all.sort { |a, b| a.name <=> b.name }
+      if input.to_i < song_list.length && input.to_i > 0
+        index = input.to_i - 1
+        if song_list[index] != nil
+          puts "Playing #{song_list[index].name} by #{song_list[index].artist.name}"
+        end
+      end
     end
   end
 
