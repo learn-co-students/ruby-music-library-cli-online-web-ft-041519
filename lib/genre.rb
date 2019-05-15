c459227e194c792a6407ad720e1057e7236ebe56
Annia Forney <anniaforney@gmail.com>
2019-05-15 22:13:59 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,29 @@
+class Genre
+  attr_accessor :name
+  attr_reader :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+      genre = Genre.new(name)
+      genre.save
+      genre
+  end
+end
