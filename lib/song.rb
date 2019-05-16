9e111cb010d4c9dc41f40e8c4256a1815df872e5
Oscar Shoudt <shoudto@gmail.com>
2019-05-16 01:50:35 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -65,4 +65,8 @@ class Song
    genre = Genre.find_or_create_by_name(genre_name)
    Song.new(song_name, artist, genre)
   end 
+  
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename).save 
+  end 
 end 
