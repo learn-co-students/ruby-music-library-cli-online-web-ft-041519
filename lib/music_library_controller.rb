9fc6f19f337147972cdf17a858bacf2011bd4778
BethAnn Hanlon <herbivore137@gmail.com>
2019-05-17 08:38:12 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,7 +1,101 @@
+require 'pry'
+require_relative './concerns/findable'
+
 class MusicLibraryController
+  extend Concerns::Findable
   
-  def initialize(path)
+  def initialize(path = "./db/mp3s")
+    music_importer = MusicImporter.new(path)
+    music_importer.import
+  end
+  
+  def call
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+    input = gets while input != "exit"
+  end
+  
+  # CLI Methods
+  
+  def list_songs
+    number = 1
+    # list = Song.all.sort_by{|song| song.name}
+    # list.each do |song|   
+    Song.all.sort_by!{|s| s.name}.each do |song|
+      puts "#{number}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+      number += 1
+    end
+  end
+  
+  def list_artists
+    number = 1
+    Artist.all.sort_by{|a| a.name}.each do |artist|
+      puts "#{number}. #{artist.name}"
+      number += 1
+    end
+  end
+  
+  def list_genres
+    number = 1
+    Genre.all.sort_by{|g| g.name}.each do |genre|
+      puts "#{number}. #{genre.name}"
+      number += 1
+    end
+  end
+  
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    artist = gets
+    number = 1
+    Artist.find_or_create_by_name(artist).songs.sort_by{|s| s.name}.each do |song|
+      puts "#{number}. #{song.name} - #{song.genre.name}"
+      number += 1
+    end
+  end
+  
+  # list artist & song, alphabetized by song name
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    genre = gets
+    number = 1
+    Genre.find_or_create_by_name(genre).songs.sort_by{|s| s.name}.each do |song|
+      puts "#{number}. #{song.artist.name} - #{song.name}"
+      number += 1
+    end
+  end
+  
+  def play_song
+    puts "Which song number would you like to play?"
+    song_num = gets
+    song = arr[song_num.to_i - 1]
+    # if song_num < arr.size && song_num > 0
+    #   song = Song.find_by_name(arr[song_num - 1])
+    #   puts "Playing #{song.name} by #{song.artist.name}" 
+    # end
     
+    
+    # song = list_songs[song_num - 1]
+    # if song is found, puts song
+    # elsif song is not found check input
+    
+    # if Song.find_by_name(song)
+    #   puts "Playing #{song.name} by #{song.artist.name}" 
+    # else
+    #   puts "Please enter a valid option"
+    #   play_song
+      # if song_num >= list_songs.size - 1 # validates_each
+        
+      # else
+      #   play_song
+      # end
+    # end
   end
   
 end
