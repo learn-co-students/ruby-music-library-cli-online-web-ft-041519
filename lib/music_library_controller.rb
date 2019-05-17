f18ab403edefacdf7c50488b2e1310ccde2995e1
BethAnn Hanlon <herbivore137@gmail.com>
2019-05-17 23:17:24 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -4,8 +4,6 @@ require_relative './concerns/findable'
 class MusicLibraryController
   extend Concerns::Findable
   
-  attr_accessor :songs
-  
   def initialize(path = "./db/mp3s")
     music_importer = MusicImporter.new(path)
     music_importer.import
@@ -13,29 +11,45 @@ class MusicLibraryController
   
   def call
     puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
-    input = gets while input != "exit"
-  end
+    
+    input = "" 
+    while input != "exit"
+     puts "To list all of your songs, enter 'list songs'."
+     puts "To list all of the artists in your library, enter 'list artists'."
+     puts "To list all of the genres in your library, enter 'list genres'."
+     puts "To list all of the songs by a particular artist, enter 'list artist'."
+     puts "To list all of the songs of a particular genre, enter 'list genre'."
+     puts "To play a song, enter 'play song'."
+     puts "To quit, type 'exit'."
+     puts "What would you like to do?"
+    
+      # CLI Commands
   
+      input = gets.chomp
+      case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+         play_song
+      end
+    end
+  end
   # CLI Methods
   
   def list_songs
     number = 1
-    # list = Song.all.sort_by{|song| song.name}
-    # list.each do |song|   
     Song.all.sort_by!{|s| s.name}.each do |song|
-      @songs << song
       puts "#{number}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
       number += 1
     end
-    # return value = array of alphabetized Song objects
   end
   
   def list_artists
@@ -64,7 +78,6 @@ class MusicLibraryController
     end
   end
   
-  # list artist & song, alphabetized by song name
   def list_songs_by_genre
     puts "Please enter the name of a genre:"
     genre = gets
@@ -76,13 +89,16 @@ class MusicLibraryController
   end
   
   def play_song
-    # songs = list songs
     puts "Which song number would you like to play?"
+    songs = Song.all.sort_by!{|s| s.name}
     song_num = gets.chomp.to_i
-    if song_num > 0 && song_num <= @songs.size
-      song = @songs[song_num - 1]
+    if song_num > 0 && song_num <= songs.size
+      song = songs[song_num - 1]
       puts "Playing #{song.name} by #{song.artist.name}" 
     end    
   end
   
+
+  
 end
+  
